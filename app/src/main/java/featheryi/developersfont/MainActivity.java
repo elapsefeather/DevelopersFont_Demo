e5b443f6aeae8b874a83ff07476c08f473af15f5
feather <e25824564@gmail.com>
2018-05-09 10:40:47 +0800

    Android Studio System Fonts Demo

---
+++ app/src/main/java/featheryi/developersfont/MainActivity.java
@@ -0,0 +1,25 @@
+package featheryi.developersfont;
+
+import android.graphics.Typeface;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*
+
+        1. 新建 TextView
+        2. 設置屬性 fontFamily
+        3. 可至最下方下載新字型
+         （下載的手機不支援就不能用 幾乎）
+
+        */
+    }
+}
